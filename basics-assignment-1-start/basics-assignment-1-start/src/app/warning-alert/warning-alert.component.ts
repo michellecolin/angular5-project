e2cd5c5f1b1ac5fe7dd6031641eb9aae68aa9091
michelle colin <michelle.c.colin@gmail.com>
2018-04-02 10:10:48 -0300

    Two course tasks

---
+++ basics-assignment-1-start/basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: '<div>warning alert</div>',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
+import { ServerComponent } from './server/server.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -42,13 +43,16 @@ import {
   MatToolbarModule,
   MatTooltipModule,
 } from '@angular/material';
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
