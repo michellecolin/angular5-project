07d1e434f180a8b33acff89069a0df2123ce02a9
michelle colin <michelle.c.colin@gmail.com>
2018-04-03 15:43:24 -0300

    Show recipe details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() openRecipeDetails = new EventEmitter<{recipe}>();
+
   recipes: Recipe[] = [
     new Recipe('lasagna', 'pasta with ground meet', 'https://images.media-allrecipes.com/userphotos/560x315/3359675.jpg'),
-    new Recipe('lasagna', 'pasta with ground meet', 'https://images.media-allrecipes.com/userphotos/560x315/3359675.jpg')
+    new Recipe('gnocchi', 'gnocchi with ground meet', 'enfants.teamwork.com/#/projects/185389/tasks/board')
   ];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  showRecipeDetails(recipe) {
+    console.log('showRecipeDetails event', recipe);
+    this.openRecipeDetails.emit({recipe: recipe});
+  }  
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+  recipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openRecipeDetails(event) {
+    this.recipe = event.recipe;
+    console.log('openRecipeDetails', event);
   }
 
 }
